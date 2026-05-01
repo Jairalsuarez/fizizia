435ff68cd35afbab51374538452656c445b38cdc
Jaira Suarez <jairsuarez9806@gmail.com>
2026-05-01 08:42:48 -0500

    Split admin into standalone app

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
-import { useEffect, useState } from 'react'
-import { Capacitor } from '@capacitor/core'
 import { AdminApp } from './components/admin/AdminApp'
-import { Footer } from './components/Footer'
-import { Header } from './components/Header'
-import { CtaSection } from './components/landing/CtaSection'
-import { HeroSection } from './components/landing/HeroSection'
-import { ProcessSection } from './components/landing/ProcessSection'
-import { ProjectsSection } from './components/landing/ProjectsSection'
-import { ServicesSection } from './components/landing/ServicesSection'
-import { TestimonialSection } from './components/landing/TestimonialSection'
 
 function App() {
-  const [hash, setHash] = useState(window.location.hash)
-  const isNativeApp = Capacitor.isNativePlatform()
-
-  useEffect(() => {
-    function handleHashChange() {
-      setHash(window.location.hash)
-    }
-
-    window.addEventListener('hashchange', handleHashChange)
-    return () => window.removeEventListener('hashchange', handleHashChange)
-  }, [])
-
-  if (isNativeApp || hash === '#/admin' || hash === '#/login') {
-    return <AdminApp />
-  }
-
-  return (
-    <main>
-      <Header />
-      <section className="hero-section">
-        <HeroSection />
-      </section>
-      <ServicesSection />
-      <ProjectsSection />
-      <ProcessSection />
-      <TestimonialSection />
-      <CtaSection />
-      <Footer />
-    </main>
-  )
+  return <AdminApp />
 }
 
 export default App
